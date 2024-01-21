b060bc889f5f68912a80aa9b56566cc969b98896
harsha vardhan <harsha.vardhan@hashteksolutions.com>
2024-01-21 14:55:05 +0530

    added register button

--- Low-Bridge-Softwares-WebApp/outsystem/settings.py
+++ Low-Bridge-Softwares-WebApp/outsystem/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'outsystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('POSTGRES_NAME'),
+#         'USER': os.environ.get('POSTGRES_USER'),
+#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
 
 
 # Password validation
